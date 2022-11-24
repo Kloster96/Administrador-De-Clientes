162aef3cf64f652f408046e889ee21667d331a9d
Kloster <luciano.kloster@hotmail.com>
2022-11-24 18:16:08 -0300

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({children}) => {
+  return (
+    <div className="text-center bg-red-700 text-white font-bold p-3 uppercase">
+        {children}
+    </div>
+  )
+}
+
+export default Error
